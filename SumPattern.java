eee20b952ecf06402a3829b9046095652a3487c2
Bhawana Gaur <116781114+ibhawanagaur@users.noreply.github.com>
2022-10-31 09:19:15 +0530

    Update SumPattern.java

--- SumPattern.java
+++ SumPattern.java
@@ -33,30 +33,3 @@ public class SumPattern {
 		}
 	}
 }
-/*
-        Scanner s=new Scanner(System.in);
-		int n=s.nextInt();
-		int i=1;
-		int sum=i;    
- 		 while(i<=n){
-		int t=1;    //starting number    
-        int j=1;
-		while(j<=i){                    
-         	if(t<i){
-			System.out.print(t+"+");            
-  			 t++;                                          
-   			j++;
- }              
-
-else{            
-       System.out.print(t+"="+sum);            
-        j++;
-                      }
-          }        
-   System.out.println();    
-      i++;      
-     sum=sum+i;
-        }
-    }
-    
-}*/
